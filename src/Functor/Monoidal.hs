f30f09f9c5d1ddd4958abf954aa35a70d3c5b899
Hexirp <Hexirp@users.noreply.github.com>
2017-03-18 13:17:22 +0900

    Add Functor.Monoidal

---
+++ src/Functor/Monoidal.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE KindSignatures #-}
+
+module Functor.Monoidal
+  ( Monoidal(Monoidal)
+  , sourceMonoidal
+  , targetMonoidal
+  , extend_functor
+  , unit
+  , product
+  ) where
+  import qualified Category.Monoidal (Monoidal) as C
+
+  data Monoidal :: (* -> * -> *) -> (* -> * -> *) -> * -> (* -> * -> *) -> (* -> * -> *) -> * -> (* -> *) -> * where
+    Monoidal ::
+      { sourceMonoidal :: C.Monoidal cat pro i
+      , targetMonoidal :: C.Monoidal dat qro j
+      , extend_functor :: Functor cat dat f
+      , unit :: dat j (f i)
+      , product :: (forall a b. qro (f a) (f b) -> f (pro a b))
+      } -> Monoidal cat pro i dat qro j f

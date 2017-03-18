1d00d6836bb9ca1deaa79dd6d79bd967cd9f7a4f
Hexirp <Hexirp@users.noreply.github.com>
2017-03-18 09:39:51 +0900

    Add PFunctor

---
+++ src/Functor/PFunctor.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE LiberalTypeSynonyms #-}
+
+module PFunctor
+  ( PFunctor(PFunctor)
+  , pfunctor
+  , extend_funcor
+  ) where
+  import Functor
+
+  type Flip t a b = t b a
+
+  data PFunctor :: (* -> * -> *) -> (* -> * -> *) -> (* -> * -> *) -> * where
+    PFunctor ::
+      { extend_functor :: (forall a. Functor cat dat (Filp pro a) f)
+      } -> PFunctor cat dat pro f

052b01a6c39f2810721036604f48452fec425174
Hexirp <Hexirp@users.noreply.github.com>
2017-04-01 12:55:40 +0900

    Fix Functor.Adjoint

--- src/Functor/Adjoint.hs
+++ src/Functor/Adjoint.hs
@@ -7,9 +7,11 @@ module Functor.Adjoint where
   import Functor (Functor)
   import Isomorphism (Isomorphism)
 
+  type Hask = (->)
+
   data Adjoint :: (* -> * -> *) -> (* -> * -> *) -> (* -> *) -> (* -> *) -> * where
     Adjoint ::
       { leftFunctor :: Functor cat dat f
       , rightFunctor :: Functor dat cat g
-      , adjunct :: (forall a b. Isomorphism (dat (f a) b) (cat a (g b)))
+      , adjunct :: (forall a b. Isomorphism Hask (dat (f a) b) (cat a (g b)))
       } -> Adjoint cat dat f g

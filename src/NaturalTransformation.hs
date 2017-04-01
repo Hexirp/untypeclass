a0f09290effe3688a17f28e3e6ee384d8bc21aae
Hexirp <Hexirp@users.noreply.github.com>
2017-04-01 14:20:22 +0900

    Add NaturalTransformation

---
+++ src/NaturalTransformation.hs
@@ -0,0 +1,15 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE KindSignatures #-}
+
+module NaturalTransformation where
+  import Functor (Functor)
+
+  -- natural . sourceFunctor f = targetFunctor f . natural
+  data NaturalTransformation :: (* -> * -> *) -> (* -> * -> *) -> (* -> *) -> (* -> *) -> * where
+    NaturalTransformation ::
+      { sourceFunctor :: Functor cat dat f
+      , targetFunctor :: Functor cat dat g
+      , natural :: (forall a. dat (f a) (g a))
+      } -> NaturalTransformation cat dat f g

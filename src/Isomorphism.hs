d7206e604a42f35131319585fee8c856f9863aba
Hexirp <Hexirp@users.noreply.github.com>
2017-04-01 13:00:49 +0900

    Fix Isomorphism

--- src/Isomorphism.hs
+++ src/Isomorphism.hs
@@ -7,9 +7,14 @@ module Isomorphism
   , to
   , from
   ) where
+  import Category (Category)
 
+  -- (.) = compose $ category #
+  -- id' = id $ category #
+  -- to . from = from . to = id'
   data Isomorphism :: (* -> * -> *) -> * -> * -> * where
     Isomorphism ::
-      { to :: cat a b
+      { category :: Category cat
+      , to :: cat a b
       , from :: cat b a
       } -> Isomorphism cat a b

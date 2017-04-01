1eeee4f26d4b289b5472bd2b040d02c09e590068
Hexirp <Hexirp@users.noreply.github.com>
2017-04-01 15:33:47 +0900

    Add Functor.Endo

---
+++ src/Functor/Endo.hs
@@ -0,0 +1,12 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE KindSignatures #-}
+
+module Functor.Endo where
+  import Functor (Functor)
+
+  data Endo :: (* -> * -> *) -> * where
+    Endo ::
+      { extend_functor -> Functor cat cat f
+      } -> Endo cat f

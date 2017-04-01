c518234e42108039419e8401b3a2edcdb512d4ff
Hexirp <Hexirp@users.noreply.github.com>
2017-04-01 17:49:53 +0900

    Add High.Category.Monoidal

---
+++ src/High/Category/Monoidal.hs
@@ -0,0 +1,16 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE KindSignatures #-}
+
+module High.Category.Monoidal where
+  import High.Category.Semigroupal (Semigroupal)
+  import High.Isomorphism (Isomorphism)
+
+  data Monoidal :: ((* -> *) -> (* -> *) -> *) -> ((* -> *) -> (* -> *) -> (* -> *)) -> (* -> *) -> * where
+  Monoidal ::
+    { extend_semigroupal :: Semigroupal cat pro
+    , tensor_unit :: (forall a. i a)
+    , left_unitor :: (forall a. Isomorphism cat (pro i a) a)
+    , right_unitor :: (forall a. Isomorphism cat (pro a i) a)
+    } -> Monoidal cat pro i

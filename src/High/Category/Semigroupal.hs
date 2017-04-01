826cdd3152413d39bf75d6bac859304600695949
Hexirp <Hexirp@users.noreply.github.com>
2017-04-01 17:19:35 +0900

    Add Semigroupal

---
+++ src/High/Category/Semigroupal.hs
@@ -0,0 +1,13 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE KindSignatures #-}
+
+module High.Category.Semigroupal where
+  import High.Functor.Bifunctor (Bifunctor)
+  import Isomorphism (Isomorphism)
+
+  data Semigroupal :: ((* -> *) -> (* -> *) -> *) -> ((* -> *) -> (* -> *) -> (* -> *)) -> * where
+    Semigroupal ::
+      { tensor_product :: Bifunctor cat cat cat pro
+      , associator :: (forall f g h. Isomorphism cat (pro (pro f g) h) (pro f (pro g h)))}

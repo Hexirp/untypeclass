c01b6e4ac0a7c3328594d98e4e6874a3c13ef88d
Hexirp <Hexirp@users.noreply.github.com>
2017-04-01 18:03:44 +0900

    Fix High.Category.Monoidal

--- src/High/Category/Monoidal.hs
+++ src/High/Category/Monoidal.hs
@@ -10,7 +10,7 @@ module High.Category.Monoidal where
   data Monoidal :: ((* -> *) -> (* -> *) -> *) -> ((* -> *) -> (* -> *) -> (* -> *)) -> (* -> *) -> * where
     Monoidal ::
       { extend_semigroupal :: Semigroupal cat pro
-      , tensor_unit :: (forall a. i a)
+      , tensor_unit :: (forall a. a -> i a)
       , left_unitor :: (forall a. Isomorphism cat (pro i a) a)
       , right_unitor :: (forall a. Isomorphism cat (pro a i) a)
       } -> Monoidal cat pro i

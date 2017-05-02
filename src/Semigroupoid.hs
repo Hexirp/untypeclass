827d4644720c857db0ea1e8bade606496905af21
Hexirp <Hexirp@users.noreply.github.com>
2017-05-02 09:29:54 +0900

    Update Semigroupoid

--- src/Semigroupoid.hs
+++ src/Semigroupoid.hs
@@ -4,13 +4,7 @@
 {-# LANGUAGE KindSignatures #-}
 
 module Semigroupoid
-  ( Semigroupoid(Semigroupoid)
-  , compose
+  ( Semigroupoid
   ) where
 
-  -- > (.) = compose #
-  -- > x . (y . z) = (x . y) . z
-  data Semigroupoid :: (* -> * -> *) -> * where
-    Semigroupoid ::
-      { compose :: (forall a b c. cat b c -> cat a b -> cat a c)
-      } -> Semigroupoid cat
+  type Semigroupoid cat = forall a b c. cat b c -> cat a b -> cat a c

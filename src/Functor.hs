acd7b41ae1d7a180312a3860ce980e190e0cf389
Hexirp <Hexirp@users.noreply.github.com>
2017-05-12 16:32:18 +0900

    Add 'fromCategory'

--- src/Functor.hs
+++ src/Functor.hs
@@ -5,10 +5,26 @@
 
 module Functor
   ( Functor
+  , fromCategory
+  , liftComposing
   ) where
-  import Data.Either (Either)
-  import Category (Composing)
+  import Control.Arrow ((&&&))
+  import Data.Either (Either, either)
+  import Data.Function ((.), ($))
+  import Category (Category, Composing(..), composing)
   
   type Functor cat dat f = forall a b. (
     Composing cat a b -> (cat a b, Composing dat (f a) (f b)),
     Either (cat a b) (Composing dat (f a) (f b)) -> dat (f a) (f b))
+    
+  fromCategory
+    :: Category cat
+    -> Category dat
+    -> (forall a b. cat a b -> dat (f a) (f b))
+    -> Functor cat dat f
+  fromCategory c d f = (c &&& liftComposing f, either f d)
+  
+  liftComposing
+    :: (forall a b. cat a b -> dat (f a) (f b))
+    -> Composing cat a b -> Composing dat (f a) (f b)
+  liftComposing = composing Id $ Composed . f
